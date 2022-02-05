528c5615fd039c7cf4d12f4893dfd4405b0bacd2
Anthony Zamarro <azamarro@veracode.com>
2022-02-05 17:00:48 -0500

    further configure webpack.config.js and load React app with npm start script

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
     mode: "development",
-    devServer: {
-        static: './dist',
-        hot: true,
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            }
+        ]
     },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html')
+        })
+    ]
 };
