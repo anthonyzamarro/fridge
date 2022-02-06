bd04be41fefbf70e4e32e0ebd78e2b1240e34c22
Anthony Zamarro <azamarro@veracode.com>
2022-02-06 10:32:17 -0500

    add react refresh and hmr

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './styles.css';
 import IMAGE from './owl.jpg';
 import SVG from './people.svg';
+import { ClickCounter } from './ClickCounter';
+
 
 export const App = () => {
     return (
         <>
-            <h1>React {process.env.NODE_ENV}</h1>
+            <h1>Hello, React {process.env.NODE_ENV}</h1>
             <img src={IMAGE} alt="owl" width={300} height={200} />
             <img src={SVG} alt="people" width={300} height={200} />
+            <ClickCounter />
         </>
     )
 }
