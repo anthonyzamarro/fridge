7e142d59c490cf95ff84833c2e4e363058126f41
Anthony Zamarro <azamarro@veracode.com>
2022-02-06 09:45:03 -0500

    add image, css, and svg rules to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './styles.css';
+import IMAGE from './owl.jpg';
+import SVG from './people.svg';
 
 export const App = () => {
     return (
-        <h1>React</h1>
+        <>
+            <h1>React</h1>
+            <img src={IMAGE} alt="owl" width={300} height={200} />
+            <img src={SVG} alt="people" width={300} height={200} />
+        </>
     )
 }
