a12891feb90574de1ae543bbe474c467c75fcf7d
Anthony Zamarro <azamarro@veracode.com>
2022-02-06 10:04:27 -0500

    add prod and dev mode for webpack

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SVG from './people.svg';
 export const App = () => {
     return (
         <>
-            <h1>React</h1>
+            <h1>React {process.env.NODE_ENV}</h1>
             <img src={IMAGE} alt="owl" width={300} height={200} />
             <img src={SVG} alt="people" width={300} height={200} />
         </>
