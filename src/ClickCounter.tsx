753d274e63c4797e22b79dd94b791059030ca412
Anthony Zamarro <azamarro@veracode.com>
2022-02-06 11:31:40 -0500

    testing husky

--- src/ClickCounter.tsx
+++ src/ClickCounter.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 
 export const ClickCounter = () => {
-  const blah = 'hi'
   const [count, setCount] = useState(0)
   return (
     <div>
-      <p>{blah}</p>
       <button onClick={() => setCount((c) => c + 1)}>Count {count} </button>
     </div>
   )
