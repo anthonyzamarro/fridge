6d46aefc168ae6d2213c46adec8b6068fee95842
Anthony Zamarro <anthony.zamarro@gmail.com>
2022-02-17 22:04:24 -0500

    reworking data shapes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,22 @@ import FoodList from './components/FoodList/FoodList';
 
 import './styles.css';
 
-interface FoodListArrayProps {
+interface FoodListProps {
   id: number;
-  list: object[];
+  foods: [];
 }
 
+interface FoodListArrayProps {
+  // id: number;
+  list: FoodListProps;
+}
+
+// const keys = Object.keys(localStorage).map((key) => key);
+// const values = Object.keys(localStorage).map((key) => localStorage[key]);
+// console.log(keys, values);
+
 export const App = () => {
-  const [foodListId, setFoodListId] = useState<number>(0);
+  // const [foodListId, setFoodListId] = useState<number>(0);
   const [foodList, setFoodList] = useState<FoodListArrayProps[]>([]);
 
   const handleClickAddFoodList = () => {
@@ -22,26 +31,56 @@ export const App = () => {
         /*
           spread existing keys into food list and add new items
         */
-        setFoodList([...foodList, { id: foodId, list: localStorage[key] }]);
+        // setFoodList([...foodList, { id: foodId, list: localStorage[key] }]);
+        console.log(localStorage[key]);
+        setFoodList([
+          ...foodList,
+          {
+            list: {
+              id: foodId,
+              foods: localStorage[key],
+            },
+          },
+        ]);
+        localStorage.setItem(
+          `${foodId}`,
+          JSON.stringify({ list: { id: foodId, foods: localStorage[key] } })
+        );
       });
-      setFoodListId(foodId);
+      // setFoodListId(foodId);
     } else {
       setFoodList([
         ...foodList,
         {
-          id: foodListId,
-          list: [],
+          list: {
+            id: 0,
+            foods: [],
+          },
         },
       ]);
-      setFoodListId(foodListId + 1);
+
+      localStorage.setItem(
+        `${0}`,
+        JSON.stringify({ list: { id: 0, foods: [] } })
+      );
+      // setFoodList([
+      //   ...foodList,
+      //   {
+      //     id: 0,
+      //     list: [],
+      //   },
+      // ]);
+      // setFoodListId(foodListId + 1);
     }
   };
 
   const updateFoodList = () => {
-    const foods = Object.keys(localStorage).map((key) => {
-      return { id: parseInt(key), list: localStorage[key] };
-    });
-    setFoodList([...foods]);
+    // const foods = Object.keys(localStorage).map((key) => {
+    //   return { list: { id: key, foods: localStorage[key] } };
+    // });
+    // console.log(foods);
+    // setFoodList([...foods]);
+    // console.log(localStorage);
   };
 
   const handleClickDeleteFoodList = (foodId: number) => {
@@ -68,11 +107,11 @@ export const App = () => {
 
       <div className="food-list__container">
         {foodList.length > 0 &&
-          foodList.map((list) => {
+          foodList.map(({ list }) => {
             return (
               <FoodList
                 key={list.id}
-                id={list.id}
+                id={0}
                 deleteFoodList={handleClickDeleteFoodList}
               />
             );
@@ -91,4 +130,12 @@ export const App = () => {
  * see food items in a list
  * edit food item in a list
  * delete food item in a list
+ * list {
+ *  id: number,
+ *  items: [
+ *    { title: string,
+ *      foods: [{name: string, group: string, expire: date, price: number}]
+ *    }
+ *  ]
+ * }
  */
