5cf94f554d9f250b6602b0e364d1f35d29909a38
Anthony Zamarro <anthony.zamarro@gmail.com>
2022-03-15 22:12:02 -0400

    move dateFormat code into separate directory. add files for expirationTimer and notification functions along with outline comments

---
+++ src/utils/expirationTimer/expirationTimer.ts
@@ -0,0 +1,5 @@
+export const expirationTimer = () => {
+  // timer function that is started whenever new food item is added to a list
+  // will need to predict expiration of food item based on what the food is
+  // and when it was added to the list
+};
---
+++ src/utils/notifyUser/notifyUser.ts
@@ -0,0 +1,4 @@
+export const notifyUser = () => {
+  // send notification to user about which food items will expire soon
+  // default notification day is Sunday morning, but user can adjust as they see fit
+};
